36133d4ea557260bd414ab8fc6470960755b52cf
Hughlekrogan <hugo.maurer@hotmail.fr>
2013-01-16 20:45:56 +0100

    Pause!

--- TurkeySmash/Code/Menu/Pause.cs
+++ TurkeySmash/Code/Menu/Pause.cs
@@ -22,8 +22,10 @@ namespace TurkeySmash
 
             public Pause()
             {
+                xPos = TurkeySmashGame.manager.PreferredBackBufferWidth / 4;
+                yPos = TurkeySmashGame.manager.PreferredBackBufferHeight / 4;
                 bouton1 = new BoutonMenu(xPos, yPos + 100);
-                bouton2 = new BoutonMenu(xPos, yPos + 300);
+                bouton2 = new BoutonMenu(xPos, yPos + 200);
                 bouton1.Texte = "Retour";
                 bouton2.Texte = "Menu Principal";
             }
